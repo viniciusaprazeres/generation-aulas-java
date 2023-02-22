c5159022d1d82c43abd56fe900a0c81710a9b99f
Vinicius Prazeres <107568409+viniciusaprazeres@users.noreply.github.com>
2023-02-22 18:02:12 -0300

    Aula 08 - Programação orientada a objetos 2

---
+++ aula_08/src/mtransporte/Terrestre.java
@@ -0,0 +1,36 @@
+package mtransporte;
+
+public class Terrestre extends Transporte {
+
+    private int nRodas;
+    private float velocidade;
+
+    public Terrestre(int capacidade, int nRodas, float velocidade) {
+        super(capacidade);
+        this.nRodas = nRodas;
+        this.velocidade = velocidade;
+    }
+
+    public int getnRodas() {
+        return nRodas;
+    }
+
+    public void setnRodas(int nRodas) {
+        this.nRodas = nRodas;
+    }
+
+    public float getVelocidade() {
+        return velocidade;
+    }
+
+    public void setVelocidade(float velocidade) {
+        this.velocidade = velocidade;
+    }
+
+    public void visualizar(){
+        super.visualizar();
+        System.out.println("Número de rodas: " + this.nRodas);
+        System.out.println("Velocidade: " + this.velocidade);
+    }
+
+}
